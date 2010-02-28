45e7ada057a4acee879539fbcbb4d12508a40665
Paul Molodowitch <elrond79@gmail.com>
2010-02-28 11:16:42 -0800

    changes to test_PM_heatWeight.py

--- src/test_PM_heatWeight.py
+++ src/test_PM_heatWeight.py
@@ -1,9 +1,14 @@
 import PM_heatWeight
 
-import maya.standalone
-maya.standalone.initialize()
+try:
+    import maya.standalone
+    maya.standalone.initialize()
+except RuntimeError: pass
+
 import maya.cmds as cmds
 
-cmds.file("C:/Dev/Projects/eclipse/workspace/heatWeightProject/testScenes/autoRig_test1.mb", o=True, f=True)
+cmds.file("C:/Dev/Projects/eclipse/workspace/heatWeightProject/testScenes/scenes/autoRig_test3.mb", o=True, f=True)
 cmds.select('root', 'cubeMesh')
-PM_heatWeight.heatWeight()
+PM_heatWeight.heatWeight(undoable=False,
+    tempOutputDir='C:/Dev/Projects/eclipse/workspace/heatWeightProject/src',
+    tempDelete=False)
